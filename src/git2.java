e9d7b33eae5b7c45057949147554eb6806b3dfc8
venkatsai22599 <venkatasai22599@gmail.com>
2022-10-07 12:51:58 +0530

    git2

---
+++ src/git2.java
@@ -0,0 +1,10 @@
+public class git2 {
+
+        public static void main(String[] args) {
+
+        System.out.println("git2 example 1");
+        System.out.println("git2 example 2");
+    }
+
+}
+
